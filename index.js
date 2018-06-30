25fcb21317f5889867c6a2a29a413128f536d58e
Suhit Kumar <suhit.kmr1@gmail.com>
2018-06-30 11:47:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ function happyHolidays(){
   return ('Happy holidays!')
 }
 function happyHolidaysTo(name){
-  return('Happy holidays, ${name}!')
+  var message = `Happy holidays, ${name}!`
+  return(message)
 }
 function happyHolidaysTo(holiday,name){
-  return('Happy ${holiday}, ${name}!')
+  var message = `Happy ${holiday}, ${name}!`
+  return(message)
 }
 function holidayCountdown(days,holiday){
-  return('It\'s ${days} days until ${holiday}!')
+  var message = `It\'s ${days} days until ${holiday}!`
+  return(message)
 }
